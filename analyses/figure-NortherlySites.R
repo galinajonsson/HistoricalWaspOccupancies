ce44983542652d5498a0d96d0798236f4c617bde
galinajonsson <galina.jonsson@gmail.com>
2021-01-24 16:19:25 +0000

    Moved figure code for most northerly grid cells into separate script from 03-summaries

---
+++ analyses/figure-NortherlySites.R
@@ -0,0 +1,62 @@
+### Annual most northerly grid cells
+#Here, we summarise the five most northerly grid cells recorded each year per species and plot the annual range of these grid cells' latitude
+
+# Load packages
+require(dplyr)
+require(plyr)
+require(boot)
+library("sparta")
+
+# Load required functions
+source("./analyses/function-readRDS_multi.R")
+
+
+# Load all model files
+readRDS_multi("./outputs/modelOutputs")
+
+
+# Load the formatted data used in analyses
+spp_vis <- read.csv('../outputs/formattedData/spp_vis_merged_1kmgrid_date_year.csv', header=T, na.strings=c("","NA"))
+
+
+# Subset each species into separate data frames
+spp_vis_crabro <- subset(spp_vis, Vespa.crabro == "TRUE")
+spp_vis_crabro$species <- c("Vespa crabro")
+spp_vis_germanica <- subset(spp_vis, Vespula.germanica == "TRUE")
+spp_vis_germanica$species <- c("Vespula germanica")
+spp_vis_vulgaris <- subset(spp_vis, Vespula.vulgaris == "TRUE")
+spp_vis_vulgaris$species <- c("Vespula vulgaris")
+
+
+# Subset the first six characters of the "visit" column, which corresponds to the grid cell
+spp_vis_crabro$gridcell <- substr(spp_vis_crabro$visit, start = 1, stop = 6)
+spp_vis_germanica$gridcell <- substr(spp_vis_germanica$visit, start = 1, stop = 6)
+spp_vis_vulgaris$gridcell <- substr(spp_vis_vulgaris$visit, start = 1, stop = 6)
+
+
+# Subset the characters of the "visit" column, which corresponds to year
+spp_vis_crabro$year <- as.factor(substr(spp_vis_crabro$visit, start = 14, stop = 17))
+spp_vis_germanica$year <- as.factor(substr(spp_vis_germanica$visit, start = 14, stop = 17))
+spp_vis_vulgaris$year <- as.factor(substr(spp_vis_vulgaris$visit, start = 14, stop = 17))
+
+
+# For each species, go through each row and enter the latitude
+x <- spp_vis_crabro
+x$latitude <- as.numeric(NA)
+nrow <- nrow(x)
+years <- unique(x$year)
+
+for(i in 1:nrow){
+  x[i, "latitude"] <- (gr2gps_latlon(x[i, "gridcell"]))$LATITUDE
+}
+
+
+# Subset the five most northerly grid cells 
+df.max <- ddply(x, c("year"), subset, order(latitude,decreasing=TRUE) <= 5)
+
+#df.max <- df.max[-22,]
+#df.max <- df.max[-66,]
+
+
+# Plot
+plot(as.numeric(as.character(df.max$year)), df.max$latitude, xlab = "Year", ylab="Latitude (degrees)", ylim = c(50,55))
